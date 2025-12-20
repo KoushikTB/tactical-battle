1a56a4d05eaa545e5a6dda33140a67b715e5ff71
KingThenuja <168280427+KingThenuja@users.noreply.github.com>
2025-12-20 17:17:52 -0500

    Update main.js

--- homepage/main.js
+++ homepage/main.js
@@ -1,1 +1,13 @@
+const toggleBtn = document.getElementById('modeToggle');
+  const body = document.body;
 
+  toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    // change icon
+    if (body.classList.contains('')) {
+      toggleBtn.textContent = '🌞';
+    } else {
+      toggleBtn.textContent = '🌙';
+    }
+  });
